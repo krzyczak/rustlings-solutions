d52edf9a1054212d6fba68e8546c1f90f4596b01
krzyczak <krzyczak@users.noreply.github.com>
2023-05-25 23:01:33 +0100

    chore: finish move_semantics excercises

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,12 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut x = 100;
     let y = &mut x;
+    *y += 100; // I understand that move occurs here and that's why we can later create the new
+               // reference - i.e. because y goes out of scope here.
     let z = &mut x;
-    *y += 100;
     *z += 1000;
     assert_eq!(x, 1200);
 }
